8cb582a704016ae9f10a87d82b8a6f97f41da954
azemy1 <45692993+azemy1@users.noreply.github.com>
2018-12-07 23:47:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBinzNutORbMDLdtmwoKCvip-7yo-5eai_noachIqnk4plg5mv04mywMuclK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "f8cb1511b8038f78";
+$os="android";
+$sign="7210dbe3483ec2af7be42f28e142dcf59218513cb6157df1b49ab8510b06638e";
